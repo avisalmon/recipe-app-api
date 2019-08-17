5a5e51bea1f78e6736e3f4a0e8d02d4661c84c60
user <avi.salmon@intel.com>
2019-08-17 14:07:57 +0300

    Added core app

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_succesful(self):
+        """Test creating a new user with an email is succesful"""
+        email = 'test@londonappdev.com'
+        password = 'Testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Tests that the email was normalized to lower case"""
+        email = 'test@SOMEDOMAIN.Com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Tests that the invalid email rases the proper fault error"""
+        # the code below expects to get a ValueError
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_createsupperuser(self):
+        """Tests that when a superuser is crated the is_staff is valid. """
+        user = get_user_model().objects.create_superuser(
+            'test@someweb.org',
+            'test123'
+            )
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
